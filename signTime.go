71de4d5345d294681434c100672873dc17346882
jakevc <jvancamp@uoregon.edu>
2018-08-23 22:21:04 -0500

    working echo

--- signTime.go
+++ signTime.go
@@ -8,38 +8,38 @@ import (
 	"net/http"
 )
 
-func main() {
-
+func slashHandler(w http.ResponseWriter, r *http.Request) {
 	verificationToken := "WSSPF87NgO5USa49IfYTVlnn"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		s, err := slack.SlashCommandParse(r)
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if !s.ValidateToken(verificationToken) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	switch s.Command {
+	case "/signtime":
+		fmt.Println(s.Text)
+		params := &slack.Msg{Text: s.Text}
+		b, err := json.Marshal(params)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(b)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
 
-		if !s.ValidateToken(verificationToken) {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		switch s.Command {
-		case "/signtime":
-			fmt.Println(s.Text)
-			params := &slack.Msg{Text: s.Text}
-			b, err := json.Marshal(params)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(b)
-		default:
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	})
-
+func main() {
+	http.HandleFunc("/", slashHandler)
 	fmt.Println("[INFO] Server listening")
 	http.ListenAndServe(":5038", nil)
 }
